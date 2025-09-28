41ba1289ff1ecaa594df3a3a8b63c4ae32f024b9
Harshit Arora <harshitarora1065@gmail.com>
2025-09-28 14:53:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
     extend: {
       colors: {
         purple: {
-          50: '#f5f3ff',
-          400: '#a78bfa',
-          500: '#8b5cf6',
-          600: '#7c3aed',
-          700: '#6d28d9',
-          900: '#4c1d95',
+          50:  '#eff6ff',   // blue-50
+          400: '#60a5fa',   // blue-400
+          500: '#3b82f6',   // blue-500
+          600: '#2563eb',   // blue-600
+          700: '#1d4ed8',   // blue-700
+          900: '#1e3a8a',   // blue-900
         },
       },
     },
   },
   plugins: [],
-} 
+}
